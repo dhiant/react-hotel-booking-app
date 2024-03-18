d558d7da8d854f7e8133deff28290d3ae7a16c62
sushant dhimal <sushant07.dhimal@gmail.com>
2024-03-18 20:08:00 +0545

    add: implement Vigenere Cipher

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -2,6 +2,7 @@ import { encrypt } from "../algorithms/AES_Cipher.js";
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import { vigenereCipherEncrypt } from "../algorithms/Vigenere_Cipher.js";
 
 // Load environment variables
 dotenv.config();
@@ -11,7 +12,8 @@ export const register = async (req, res, next) => {
 	try {
 		let originalPassword = req.body.password;
 		// encrpt the original password before sending it into database
-		let hashedPassword = encrypt(originalPassword);
+		// let hashedPassword = encrypt(originalPassword);
+		let hashedPassword = vigenereCipherEncrypt(originalPassword);
 		const newUser = new User({ ...req.body, password: hashedPassword });
 		await newUser.save();
 		res.status(200).send("New User has been created!");
@@ -30,7 +32,10 @@ export const login = async (req, res, next) => {
 		if (!user) return res.status(401).json("No user found");
 
 		// if username exists verify his/her password
-		if (user.password === encrypt(req.body.password)) {
+		if (
+			// user.password === encrypt(req.body.password)
+			user.password === vigenereCipherEncrypt(req.body.password)
+		) {
 			// create token and send it to client side
 			const { _id, isAdmin } = user._doc;
 
